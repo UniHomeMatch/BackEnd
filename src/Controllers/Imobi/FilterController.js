e37c5aacc6040ee432a6aa90954a5ead8d9dc518
giovana <132526932+GiovanaGiorgi@users.noreply.github.com>
2024-08-19 19:10:57 -0300

    Add files via upload

--- src/Controllers/Imobi/FilterController.js
+++ src/Controllers/Imobi/FilterController.js
@@ -12,7 +12,7 @@ export default{
                 }
             })
             if(!imobi){
-                return response.status(404).json({message: "Nenhum imóvel com este título foi encontrado."});
+                return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
             }
             return response.json(imobi);
         } catch(error){
@@ -22,17 +22,91 @@ export default{
 
     async findByPrice(request, response) {
         try {
-            const { price } = request.params;
+            const { priceMin, priceMax } = request.params;
             const imobi = await prisma.imobi.findMany({
                 where: {
                     price: {
-                        gte: precoMin,
-                        lte: precoMax,
+                        gte: priceMin,
+                        lte: priceMax,
                     },
                 }
             })
             if(!imobi){
-                return response.status(404).json({message: "Nenhum imóvel com este preço foi encontrado."});
+                return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
+            }
+            return response.json(imobi);
+        } catch(error){
+            return response.json({message: error.message});
+        }
+    },
+
+    // async findByLocation(request, response) {
+    //     try {
+    //         const { location } = request.params;
+    //         const imobi = await prisma.imobi.findMany({
+    //             where: {
+    //                 location: {
+    //                     gte: precoMin,
+    //                     lte: precoMax,
+    //                 },
+    //             }
+    //         })
+    //         if(!imobi){
+    //             return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
+    //         }
+    //         return response.json(imobi);
+    //     } catch(error){
+    //         return response.json({message: error.message});
+    //     }
+    // },
+    
+    async findByArea(request, response) {
+        try {
+            const { areaMin, areaMax } = request.params;
+            const imobi = await prisma.imobi.findMany({
+                where: {
+                    area: {
+                        gte: areaMin,
+                        lte: areaMax,
+                    },
+                }
+            })
+            if(!imobi){
+                return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
+            }
+            return response.json(imobi);
+        } catch(error){
+            return response.json({message: error.message});
+        }
+    },
+
+    async findByBedrooms(request, response) {
+        try {
+            const { bedrooms } = request.params;
+            const imobi = await prisma.imobi.findMany({
+                where: {
+                    bedrooms: parseInt(bedrooms),
+                  }
+            })
+            if(!imobi){
+                return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
+            }
+            return response.json(imobi);
+        } catch(error){
+            return response.json({message: error.message});
+        }
+    },
+
+    async findByBathrooms(request, response) {
+        try {
+            const { bathrooms } = request.params;
+            const imobi = await prisma.imobi.findMany({
+                where: {
+                    bathrooms: parseInt(bathrooms),
+                  }
+            })
+            if(!imobi){
+                return response.status(404).json({message: "Nenhum imóvel neste parâmetro foi encontrado."});
             }
             return response.json(imobi);
         } catch(error){
